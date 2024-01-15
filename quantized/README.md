f15ddb56e303d79dc8f58d232de9aee0bfb580f6
gavinwun <gavinwun@live.com>
2024-01-15 19:50:20 +1300

    Add quantized model details and update model-quantized.py

---
+++ quantized/README.md
@@ -0,0 +1,20 @@
+
+# Model details 
+
+https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF
+```bash
+pip3 install huggingface-hub
+huggingface-cli download TheBloke/Llama-2-7b-Chat-GGUF llama-2-7b-chat.Q4_K_M.gguf --local-dir . --local-dir-use-symlinks False
+```
+
+https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF
+```bash
+pip3 install huggingface-hub
+huggingface-cli download TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf --local-dir . --local-dir-use-symlinks False
+```
+
+llama cpp python docs - https://llama-cpp-python.readthedocs.io/en/latest/api-reference/#llama_cpp.Llama
+
+# Usage
+
+Modify model-quantized.py to the model you'd like to use, and tweak the settings to fit your GPU/CPU etc.
--- quantized/model-quantized.py
+++ quantized/model-quantized.py
@@ -1,33 +1,40 @@
-# To run script below - follow instructions at https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF
+# To run script below - 
+# follow instructions at https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF
+# or https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF
+
 from llama_cpp import Llama
+import time
+from datetime import datetime
 
-# Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
+# https://llama-cpp-python.readthedocs.io/en/latest/api-reference/#llama_cpp.Llama
 llm = Llama(
-  model_path="./tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf",  # Download the model file first
-  n_ctx=2048,  # The max sequence length to use - note that longer sequence lengths require much more resources
-  n_threads=8,            # The number of CPU threads to use, tailor to your system and the resulting performance
-  n_gpu_layers=35         # The number of layers to offload to GPU, if you have GPU acceleration available
-)
+  # model_path="./tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf", 
+  model_path="./llama-2-7b-chat.Q4_K_M.gguf", 
+  chat_format="llama-2", 
+  n_ctx=1024,
+  n_threads=8,
+  n_gpu_layers=23, # This is the number of layers that will be run on GPU. The rest will be run on CPU. Tested to work on RTX 3050 with 4GB VRAM (Laptop)
+)  
 
-# Simple inference example
-output = llm(
-  "<|system|>\n{system_message}</s>\n<|user|>\n{prompt}</s>\n<|assistant|>", # Prompt
-  max_tokens=512,  # Generate up to 512 tokens
-  stop=["</s>"],   # Example stop token - not necessarily correct for this specific model! Please check before using.
-  echo=True        # Whether to echo the prompt
-)
-
-# Chat Completion API
-
-llm = Llama(model_path="./tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf", chat_format="llama-2")  # Set chat_format according to the model you are using
+# Set chat_format according to the model you are using
+start_time = time.time() 
+start_time_local = datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')
+print(f"Start time (local): {start_time_local}")
 outputs = llm.create_chat_completion(
-    messages = [
-        {"role": "system", "content": "You are a story writing assistant."},
-        {
-            "role": "user",
-            "content": "Write a story about llamas."
-        }
-    ]
+  messages = [
+      {"role": "system", "content": "You are a story writing assistant."},
+      {
+          "role": "user",
+          "content": "Write a story of exactly 1 chapter of 500 words about a super hero with crystal powers. \
+                      Also provide an image prompt for the chapter prefixed with 'image:'"
+      }
+  ],
+  temperature=0.5,
 )
 
-print(outputs)
+print(f'Chat Completion API outputs - {outputs}')
+end_time = time.time() 
+end_time_local = datetime.fromtimestamp(end_time).strftime('%Y-%m-%d %H:%M:%S')
+print(f"End time: {end_time_local}")
+total_time_seconds = (end_time - start_time)
+print(f"Total time: {total_time_seconds} seconds")
